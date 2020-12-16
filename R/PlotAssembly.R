bcf19686ed7742a1e925f86be3456d677b733744
orebas <orebas@yahoo.com>
2020-12-16 03:42:40 -0500

    progress between R and tex to produce graphs.

--- R/PlotAssembly.R
+++ R/PlotAssembly.R
@@ -4,7 +4,8 @@ library(plyr)
 library(dplyr)
 library(readr)
 library(tikzDevice)
-
+library(ggrepel)
+library(directlabels)
 
 data_all <- list.files(path = ".",     # Identify all csv files in folder
                        pattern = "temp.csv", full.names = TRUE) %>% 
@@ -30,15 +31,40 @@ for (funcName in uniqFuncs){
                     standAlone=F, 
                     width=9, 
                     height=6.5)
-  
-  myPlot <- ggplot(data=data_double[data_double$functionname == funcName,],
-                   aes( N,maxerror, color=calculator,linetype=calculator))+
+  filteredData = data_double[data_double$functionName == funcName,]
+  myPlot <- ggplot(data=filteredData,
+                   aes( N,maxError, color=calculator,linetype=calculator))+
     geom_line()+geom_point() +
-    scale_y_continuous(trans="log10", breaks = c(.1,.01,.001,1e-4,1e-5,1e-6,1e-7,1e-8,1e-9,1e-10,1e-11)) +
-    scale_x_continuous(trans="log2",breaks=c(2,4,8 ,16,32,64,128,256)) +
-    ggtitle(funcName)
+    scale_y_continuous(trans="log10", breaks = c(1.0,.1,.01,.001,1e-4,1e-5,1e-6,1e-7,1e-8,1e-9,1e-10,1e-11)) +
+    scale_x_continuous(trans="log2",breaks=c(2,4,8 ,16,32,64,128,256,512,1024)) +
+    ggtitle(funcName)+geom_dl(aes(label=calculator), method=list("maxvar.points", "bumpup",cex=0.8))+
+    scale_colour_discrete(guide="none")+
+    scale_linetype_discrete(guide="none")+
+    coord_cartesian(clip="off")
   print (myPlot)
   dev.off()
+  
+  fileTarget <- paste0({{funcName}}, "S.tex")
+  print(fileTarget)
+  tikzDevice::tikz( file=all_of(fileTarget), 
+                    standAlone=F, 
+                    width=9, 
+                    height=6.5)
+  filteredData = data_double[data_double$functionName == funcName,]
+  myPlot2 <- ggplot(data=filteredData,
+                   aes( calcHz,maxError, color=calculator,linetype=calculator))+
+    geom_line()+geom_point() +
+    scale_y_continuous(trans="log10", breaks = c(1.0,.1,.01,.001,1e-4,1e-5,1e-6,1e-7,1e-8,1e-9,1e-10,1e-11)) +
+    scale_x_continuous(trans="log10",breaks=c(.1,1,10,100,1000,10000,100000,1000000)) +
+    ggtitle(funcName)+geom_dl(aes(label=calculator), method=list("maxvar.points", "bumpup",cex=0.8))+
+    scale_colour_discrete(guide="none")+
+    scale_linetype_discrete(guide="none")+
+    coord_cartesian(clip="off")
+  print (myPlot2)
+  dev.off()
+  
+  
+  
 }
 
 
